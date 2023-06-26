ab06fabc9aa608bb0a262fe854cfbd22fc0fae61
Krylov Ivan <krylovis@mchs.ru>
2023-06-26 13:54:04 +0300

    add getUsers and getUserById

--- controllers/users.js
+++ controllers/users.js
@@ -7,3 +7,15 @@ module.exports.createUser = (req, res) => {
     .then((user) => res.send({ data: user }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
