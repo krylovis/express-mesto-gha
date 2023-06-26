5b2c415140bccf5a6f4a9114dc23b892df2bb082
Krylov Ivan <krylovis@mchs.ru>
2023-06-26 10:04:20 +0300

    add controllers

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,6 @@
 const router = require('express').Router();
-const User = require('../models/user');
+const { createUser } = require('../controllers/users');
 
-router.post('/', (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-});
+router.post('/', createUser);
 
 module.exports = router;
