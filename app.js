cca422b892b8794f5f14d78e0942c3028c5b4d72
Krylov Ivan <krylovis@mchs.ru>
2023-06-26 09:19:40 +0300

    add const users

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const mongoose = require('mongoose');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const bodyParser = require('body-parser');
 
-const { PORT = 3000 } = process.env;
 const app = express();
+const { PORT = 3000 } = process.env;
+
+const users = require('./routes/users');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use('/users', require('./routes/users'));
+app.use('/users', users);
 
 // app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => {
