73b7768ade27a971d582c29f976b41359269b1f4
Krylov Ivan <krylovis@mchs.ru>
2023-06-27 09:11:55 +0300

    add cards

--- app.js
+++ app.js
@@ -8,13 +8,23 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 const users = require('./routes/users');
+const cards = require('./routes/cards');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64992817c39a0378e2d6e2a4',
+  };
+
+  next();
+});
+
 app.use('/users', users);
+app.use('/cards', cards);
 
 // app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => {
---
+++ controllers/cards.js
@@ -0,0 +1,23 @@
+const Cards = require('../models/card');
+
+module.exports.getCards = (req, res) => {
+  Cards.find({})
+    .populate('owner')
+    .then((cards) => res.send({ data: cards }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  const { _id } = req.user;
+
+  Cards.create({ name, link, owner: _id })
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.deleteCard = (req, res) => {
+  Cards.findByIdAndRemove(req.params.id)
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
