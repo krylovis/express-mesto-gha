125031ec302f12a073c5e641319df9621f58799b
Krylov Ivan <krylovis@mchs.ru>
2023-06-29 16:45:51 +0300

    add err for cards

--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -1,4 +1,9 @@
 const Cards = require('../models/card');
+const {
+  INVALID_CARD_DATA,
+  INVALID_LIKES_DATA,
+  CARD_NOT_FOUND,
+} = require('../utils/constants');
 
 module.exports.getCards = (req, res) => {
   Cards.find({})
@@ -13,13 +18,25 @@ module.exports.createCard = (req, res) => {
 
   Cards.create({ name, link, owner: _id })
     .then((card) => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: INVALID_CARD_DATA });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   Cards.findByIdAndRemove(req.params.id)
     .then((card) => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: CARD_NOT_FOUND });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
 };
 
 module.exports.likeCard = (req, res) => {
@@ -29,7 +46,17 @@ module.exports.likeCard = (req, res) => {
     { new: true },
   )
     .then((card) => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: CARD_NOT_FOUND });
+        return;
+      }
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: INVALID_LIKES_DATA });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -39,5 +66,15 @@ module.exports.dislikeCard = (req, res) => {
     { new: true },
   )
     .then((card) => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: CARD_NOT_FOUND });
+        return;
+      }
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: INVALID_LIKES_DATA });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
 };
