af1d781deb65b4279a49ec0b34c1a41e545192b9
Krylov Ivan <krylovis@mchs.ru>
2023-06-30 08:42:58 +0300

    add constants

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,9 @@ module.exports.INVALID_USER_DATA = 'Переданы некорректные д
 module.exports.INVALID_USER_UPDATE = 'Переданы некорректные данные при обновлении профиля';
 module.exports.INVALID_AVATAR_DATA = ' Переданы некорректные данные при обновлении аватара';
 module.exports.USER_NOT_FOUND = 'Пользователь с указанным ID не найден';
+module.exports.USER_NONEXISTENT = 'Передан несуществующий ID пользователя';
 
 module.exports.INVALID_CARD_DATA = 'Переданы некорректные данные при создании карточки';
 module.exports.INVALID_LIKES_DATA = 'Переданы некорректные данные для постановки/снятии лайка';
 module.exports.CARD_NOT_FOUND = 'Карточка с указанным ID не найдена';
+module.exports.CARD_NONEXISTENT = 'Передан несуществующий ID карточки';
