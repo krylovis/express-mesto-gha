604901331d08f517ee14cef3ef4559bfb61ff3b2
Ivan Krylov <kis.226@yandex.ru>
2023-07-01 09:32:30 +0300

    add routes index

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const { HTTP_STATUS_NOT_FOUND } = require('./utils/constants');
-
 const app = express();
 const { PORT = 3000 } = process.env;
-
-const usersRoutes = require('./routes/usersRoutes');
-const cardsRoutes = require('./routes/cardsRoutes');
-
-app.use(express.json());
+const routes = require('./routes');
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(express.json());
 app.use((req, res, next) => {
-  req.user = {
-    _id: '64992817c39a0378e2d6e2a4',
-  };
-
+  req.user = { _id: '64992817c39a0378e2d6e2a4' };
   next();
 });
-
-app.use('/users', usersRoutes);
-app.use('/cards', cardsRoutes);
-app.use('*', (req, res) => res.status(404).send({ message: HTTP_STATUS_NOT_FOUND }));
+app.use(routes);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
