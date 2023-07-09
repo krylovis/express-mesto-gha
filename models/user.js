394695b2b129a03bd904c7d5aa6085cbb35f048f
Ivan Krylov <kis.226@yandex.ru>
2023-07-09 18:21:10 +0300

    fix password

--- models/user.js
+++ models/user.js
@@ -35,12 +35,14 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
 });
 
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email })
+    .select('+password')
     .then((user) => {
       if (!user) return Promise.reject(new Error(WRONG_EMAIL_OR_PASSWORD));
       return bcrypt.compare(password, user.password)
