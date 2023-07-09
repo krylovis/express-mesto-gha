4e4bd3f839b66acb75afe903325571d94ab2c812
Ivan Krylov <kis.226@yandex.ru>
2023-07-09 15:26:56 +0300

    delete hard code

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const routes = require('./routes');
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
-app.use((req, res, next) => {
-  req.user = { _id: '64992817c39a0378e2d6e2a4' };
-  next();
-});
 app.use(routes);
 
 app.listen(PORT, () => {
