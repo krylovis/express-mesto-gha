af415f1d4fc282c2b242188640a8323634ae1d0c
Krylov Ivan <krylovis@mchs.ru>
2023-07-10 14:16:12 +0300

    fix Card Schema

--- middlewares/joiSchemas.js
+++ middlewares/joiSchemas.js
@@ -38,16 +38,15 @@ module.exports.updateAvatarSchema = celebrate({
 });
 
 // Card
-
 module.exports.createCardSchema = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    link: Joi.string().regex(/^(http|https):\/\/[^ "]+$/),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(/^(http|https):\/\/[^ "]+$/),
   }),
 });
 
 module.exports.updateCardSchema = celebrate({
-  body: Joi.object().keys({
-    id: Joi.string().alphanum().length(24),
+  params: Joi.object().keys({
+    id: Joi.string().required().alphanum().length(24),
   }),
 });
