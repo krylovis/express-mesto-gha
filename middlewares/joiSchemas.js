b0bba9993b1c95ef875c9e6ced0e018c9c408af5
Krylov Ivan <krylovis@mchs.ru>
2023-07-10 13:32:24 +0300

    fix avatar valid

--- middlewares/joiSchemas.js
+++ middlewares/joiSchemas.js
@@ -4,7 +4,7 @@ module.exports.createUserSchema = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().regex(/^(http|https):\/\/[^ "]+$/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }),
