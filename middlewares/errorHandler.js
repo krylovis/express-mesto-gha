8698f65c67bde9b5607007b283f1b3e8b8c08343
Krylov Ivan <krylovis@mchs.ru>
2023-07-10 16:03:17 +0300

    add DEFAULT_ERROR

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,8 +1,10 @@
+const { DEFAULT_ERROR } = require('../utils/constants');
+
 const errorHandler = (err, req, res, next) => {
   if (err.statusCode) {
     res.status(err.statusCode).send({ message: err.message });
   } else {
-    res.status(500).send({ message: err.message || 'На сервере произошла ошибка' });
+    res.status(500).send({ message: err.message || DEFAULT_ERROR });
   }
   next();
 };
