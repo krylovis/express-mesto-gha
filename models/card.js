f33e65e2d5322650de26bbc3de6ed31dcb2ee1c5
Krylov Ivan <krylovis@mchs.ru>
2023-07-10 09:39:08 +0300

    fix cardSchema

--- models/card.js
+++ models/card.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const { WRONG_LINK_FORMAT } = require('../utils/constants');
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -10,6 +12,10 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
+    validate: {
+      validator: (value) => validator.isURL(value, { protocols: ['http', 'https'], require_tld: true, require_protocol: true }),
+      message: WRONG_LINK_FORMAT,
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
