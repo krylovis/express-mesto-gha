e456ddc64006dc60c43d102f7cfffcb4ea47e41b
Ivan Krylov <kis.226@yandex.ru>
2023-07-11 20:35:53 +0300

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,16 +5,16 @@ const { NEEDED_AUTHORIZATION } = require('../utils/constants');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-  if (!token) throw new UnauthorizedError(NEEDED_AUTHORIZATION);
+  if (!token) return next(new UnauthorizedError(NEEDED_AUTHORIZATION));
 
   let payload;
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    throw new UnauthorizedError(NEEDED_AUTHORIZATION);
+    return next(new UnauthorizedError(NEEDED_AUTHORIZATION));
   }
 
   req.user = payload;
 
-  next();
+  return next();
 };
