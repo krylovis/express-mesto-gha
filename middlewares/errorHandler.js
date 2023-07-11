c46bd6a82d878668fee428c4a8edbf0f05e1ed61
Krylov Ivan <krylovis@mchs.ru>
2023-07-11 10:14:43 +0300

    fix errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,14 @@
-const { DEFAULT_ERROR } = require('../utils/constants');
+const {
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+
+  DEFAULT_ERROR,
+} = require('../utils/constants');
 
 const errorHandler = (err, req, res, next) => {
   if (err.statusCode) {
     res.status(err.statusCode).send({ message: err.message });
   } else {
-    res.status(500).send({ message: err.message || DEFAULT_ERROR });
+    res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ DEFAULT_ERROR });
   }
   next();
 };
