539f9918139e82db49cd2acfe8dbf1f8fcd24d6f
Krylov Ivan <krylovis@mchs.ru>
2023-07-12 09:22:01 +0300

    add logger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
 const NotFoundError = require('../custom-errors/NotFoundError');
 
 const { PAGE_NOT_FOUND } = require('../utils/constants');
@@ -8,10 +9,14 @@ const usersRoutes = require('./usersRoutes');
 const cardsRoutes = require('./cardsRoutes');
 const authRoutes = require('./authRoutes');
 
+router.use(requestLogger);
+
 router.use(cookieParser());
 router.use('/', authRoutes);
 router.use('/users', usersRoutes);
 router.use('/cards', cardsRoutes);
+
+router.use(errorLogger);
 router.use(errors());
 router.use('*', (req, res, next) => next(new NotFoundError(PAGE_NOT_FOUND)));
 
