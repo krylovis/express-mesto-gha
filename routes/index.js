840aff59be44283377683a831605a1108dd1f674
Ivan Krylov <kis.226@yandex.ru>
2023-07-15 09:00:31 +0300

    add corsSettings

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const { requestLogger, errorLogger } = require('../middlewares/logger');
+const { corsSettings } = require('../middlewares/corsSettings');
 const NotFoundError = require('../custom-errors/NotFoundError');
 
 const { PAGE_NOT_FOUND } = require('../utils/constants');
@@ -11,6 +12,7 @@ const authRoutes = require('./authRoutes');
 
 router.use(requestLogger);
 
+router.use(corsSettings);
 router.use(cookieParser());
 router.use('/', authRoutes);
 router.use('/users', usersRoutes);
